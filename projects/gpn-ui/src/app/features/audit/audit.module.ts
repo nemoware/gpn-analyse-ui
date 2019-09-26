4dff732fe861e0b5a87b01babd1204c9c0958eb4
ivankrivosh <ivankrivosh.ik@gmail.com>
2019-09-26 20:23:11 +0300

    fix: формат дат dd.mm.yyyy в карточке нового аудита issue #4

--- projects/gpn-ui/src/app/features/audit/audit.module.ts
+++ projects/gpn-ui/src/app/features/audit/audit.module.ts
@@ -7,9 +7,12 @@ import { CreateAuditComponent } from './create-audit/create-audit.component';
 import { NgxMatSelectSearchModule } from '@root/node_modules/ngx-mat-select-search';
 import { ReactiveFormsModule } from '@root/node_modules/@angular/forms';
 import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
   MatFormFieldModule,
   MatSelectModule
 } from '@root/node_modules/@angular/material';
+import { APP_DATE_FORMATS, AppDateAdapter } from '@app/format/app-date-adapter';
 
 @NgModule({
   declarations: [ListAuditComponent, CreateAuditComponent],
@@ -22,6 +25,16 @@ import {
     MatFormFieldModule,
     NgxMatSelectSearchModule
   ],
-  entryComponents: [CreateAuditComponent]
+  entryComponents: [CreateAuditComponent],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: AppDateAdapter
+    },
+    {
+      provide: MAT_DATE_FORMATS,
+      useValue: APP_DATE_FORMATS
+    }
+  ]
 })
 export class AuditModule {}
