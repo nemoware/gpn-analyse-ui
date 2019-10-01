082a2e29ed844f10e919f860c101dbaf9ac00b7b
Elisey Sinkevich <selisej@yandex.ru>
2019-10-01 12:22:56 +0300

    feat: insert initial data from json
    issue #11

--- projects/server/core/initialize.js
+++ projects/server/core/initialize.js
@@ -1,23 +1,18 @@
-exports.initializeData = async db => {
-  let statuses = await db.AuditStatus.find();
-  if (statuses.length === 0) {
-    let statuses = [
-      { name: 'Новый' },
-      { name: 'В работе' },
-      { name: 'Завершен' }
-    ];
-    db.AuditStatus.insertMany(statuses);
-  }
+const fs = require('fs');
 
-  let subsidiaries = await db.Subsidiary.find();
-  if (subsidiaries.length === 0) {
-    let subsidiaries = [
-      { name: 'Газпромнефть-Восток (ООО)' },
-      { name: 'Многофункциональный комплекс Лахта центр (АО)' },
-      { name: 'Газпромнефть-Аэро (АО)' },
-      { name: 'Газпромнефть-Корпоративные продажи (ООО)' },
-      { name: 'Научно-Технический Центр «Газпром нефти» (ООО)' }
-    ];
-    db.Subsidiary.insertMany(subsidiaries);
-  }
+exports.initializeData = async db => {
+  fs.readFile(
+    './projects/server/initialData.json',
+    'utf8',
+    async (err, data) => {
+      if (err) throw err;
+      let collections = JSON.parse(data);
+      for (collection in collections) {
+        let documents = await db[collection].find();
+        if (documents.length === 0) {
+          db[collection].insertMany(collections[collection]);
+        }
+      }
+    }
+  );
 };
