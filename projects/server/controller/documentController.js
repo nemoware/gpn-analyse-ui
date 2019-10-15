99c77cdf4d0adcbe50d630cc447b3eabe7a9324f
Elisey Sinkevich <selisej@yandex.ru>
2019-10-15 17:33:43 +0300

    feat: full

--- projects/server/controller/documentController.js
+++ projects/server/controller/documentController.js
@@ -1,7 +1,6 @@
 const db = require('../config/db.config');
 const logger = require('../core/logger');
 const Document = db.Document;
-const Audit = db.Audit;
 
 exports.getDocuments = async (req, res) => {
   if (req.query.auditId == null) {
@@ -14,6 +13,29 @@ exports.getDocuments = async (req, res) => {
 
   try {
     let documents = await Document.find({ auditId: req.query.auditId });
+
+    let full;
+    if (req.query.full == null) {
+      full = true;
+    } else {
+      full = req.query.full === 'true';
+    }
+
+    if (!full) {
+      documents = JSON.parse(JSON.stringify(documents));
+      for (let document of documents) {
+        delete document['auditId'];
+        delete document['paragraphs'];
+        if (document.analysis != null) {
+          delete document.analysis['original_text'];
+          delete document.analysis['normal_text'];
+          delete document.analysis['import_timestamp'];
+          delete document.analysis['analyze_timestamp'];
+          delete document.analysis['tokenization_maps'];
+          delete document.analysis['checksum'];
+        }
+      }
+    }
     res.status(200).json(documents);
   } catch (err) {
     res.status(500).json({ msg: 'error', details: err });
