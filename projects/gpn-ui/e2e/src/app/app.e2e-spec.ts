3228525e0b1f6ac95e1de48d3310cf91db2ce736
Artem Zaborskiy <compartia@gmail.com>
2019-11-02 11:47:08 +0800

    Garbage removal

--- projects/gpn-ui/e2e/src/app/app.e2e-spec.ts
+++ projects/gpn-ui/e2e/src/app/app.e2e-spec.ts
@@ -1,16 +1,10 @@
 import { AppPage } from './app.po';
 
-import { getCurrentRouteUrl } from '../utils/utils';
 
 describe('App', () => {
   let page: AppPage;
 
   beforeEach(() => (page = new AppPage()));
-
-  it('should display current year in the footer', () => {
-    page.navigateTo();
-    expect(page.getCurrentYear()).toEqual(new Date().getFullYear().toString());
-  });
 
   it('empty menu', () => {
     page.navigateTo();
