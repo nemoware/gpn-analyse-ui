847becae2084556a7b173243b0fc684d6fd8a069
Artem Zaborskiy <compartia@gmail.com>
2019-12-09 01:48:40 +0800

    Sorting for docs 🏗 📄

--- projects/gpn-ui/src/app/features/audit/audit-analyse-result/detail/document-detail.component.ts
+++ projects/gpn-ui/src/app/features/audit/audit-analyse-result/detail/document-detail.component.ts
@@ -1,5 +1,6 @@
 import {
   Component,
+  ViewChild,
   OnInit,
   ChangeDetectionStrategy,
   Input
@@ -9,6 +10,9 @@ import { ViewDetailDoc } from '@app/models/view.detail.doc';
 import { TranslateService } from '@root/node_modules/@ngx-translate/core';
 import { DatePipe } from '@root/node_modules/@angular/common';
 import { Router } from '@root/node_modules/@angular/router';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+
 import {
   animate,
   state,
@@ -23,6 +27,14 @@ const cols_by_type = {
   'CONTRACT': ['date', 'number', 'value', 'org1', 'org2', 'contract_subject', 'analyze_state'],
   'CHARTER': ['shevron', 'date', 'org', 'analyze_state'],
   'PROTOCOL': ['date', 'org', 'org_level', 'analyze_state']
+}
+
+const column_to_sorting_mapping = {
+  'date': 'date',
+  'number': 'number',
+  'value': 'sign_value_currency/value',
+  'org1': 'org-1-name',
+  'org2': 'org-2-name', 'org': 'org-1-name'
 }
 
 
@@ -45,28 +57,44 @@ const cols_by_type = {
 })
 export class DocumentDetailComponent implements OnInit {
   @Input() documents: any;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
   header: string;
   col: string[] = [];
-  dataSource: [];
+  dataSource: MatTableDataSource<any> = new MatTableDataSource([]);
   documentType: any;
   expandedElementId = '';
   faChevronDown = faChevronDown;
   faChevronUp = faChevronUp;
   documentTypeName = null
-  
+
   constructor(
     private translate: TranslateService,
     public datepipe: DatePipe,
     private router: Router
   ) { }
-  
+
 
   isExpansionDetailRow = (i: number, row: Object) => true;
+
+
+  _sortingDataAccessor: ((data, sortHeaderId: string) => string | number) =
+    (data, sortHeaderId: string): string | number => {
+      if (sortHeaderId in column_to_sorting_mapping) {
+        const attr = column_to_sorting_mapping[sortHeaderId];
+        const res = this.getAttrValue(attr, data);
+        return res
+      }
+      return -1
+    }
 
   ngOnInit() {
 
     const docs = this.documents.docs;// shortcut
-    this.dataSource = docs;
+    // this.dataSource = new MatTableDataSource(docs);
+    this.dataSource.data = docs
+    this.dataSource.sortingDataAccessor = this._sortingDataAccessor
+    this.dataSource.sort = this.sort;
+
     this.documentTypeName = null
     if (docs && docs.length > 0) {
       this.documentTypeName = docs[0].documentType
@@ -91,7 +119,7 @@ export class DocumentDetailComponent implements OnInit {
 
   }
 
-  _isAllOrgsSame(docs, keyname: string): boolean {    
+  _isAllOrgsSame(docs, keyname: string): boolean {
     const val0 = this.getAttrValue(keyname, docs[0]) //TODO: replace with audit subsidiary name
     for (const doc of docs) {
       if (val0 !== this.getAttrValue(keyname, doc)) {
@@ -102,8 +130,10 @@ export class DocumentDetailComponent implements OnInit {
   }
 
   getAttrValue(attrName: string, doc, default_value = null) {
-    const atr = doc.attributes.find(x => x.key === attrName);
-    if (atr) return atr.value;
+    if (doc && doc.attributes) {
+      const atr = doc.attributes.find(x => x.key === attrName);
+      if (atr) return atr.value;
+    }
     return default_value;
   }
 
