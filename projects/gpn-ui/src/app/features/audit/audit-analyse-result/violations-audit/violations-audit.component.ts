5922770d6c5ad176d3ccd49f555e927d703bf1d3
Krivosh <ivankrivosh.ik@gmail.com>
2019-12-11 21:22:18 +0300

    feat: violation

--- projects/gpn-ui/src/app/features/audit/audit-analyse-result/violations-audit/violations-audit.component.ts
+++ projects/gpn-ui/src/app/features/audit/audit-analyse-result/violations-audit/violations-audit.component.ts
@@ -3,7 +3,8 @@ import {
   OnInit,
   ChangeDetectionStrategy,
   ViewChild,
-  Input
+  Input,
+  ChangeDetectorRef
 } from '@angular/core';
 import {
   MatSort,
@@ -19,23 +20,53 @@ import { ViolationModel } from '@app/models/violation-model';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ViolationsAuditComponent implements OnInit {
-  col: string[] = ['document', 'constituent', 'item', 'violation', 'base'];
-  dataSource = new MatTableDataSource();
+  col: string[] = [
+    'document',
+    'founding_document',
+    'item',
+    'violation_type',
+    'base'
+  ];
+  dataSource: MatTableDataSource<any> = new MatTableDataSource([]);
   violations: ViolationModel[];
 
   @ViewChild(MatSort, { static: true }) sort: MatSort;
   @Input() idAudit: string;
-  constructor(private auditservice: AuditService) {}
+  constructor(
+    private auditservice: AuditService,
+    private changeDetectorRefs: ChangeDetectorRef
+  ) {}
 
-  ngOnInit() {
-    this.dataSource = new MatTableDataSource([]);
-    this.dataSource.sort = this.sort;
-    this.refreshData();
+  _sortingDataAccessor: (data, sortHeaderId: string) => string | number = (
+    data,
+    sortHeaderId: string
+  ): string | number => {
+    return this.getAttrValue(sortHeaderId, data);
+  };
+
+  getAttrValue(attr, data) {
+    switch (attr) {
+      case 'violation_type': {
+        return data.violation_type;
+      }
+      case 'document': {
+        return data.document.type;
+      }
+      case 'founding_document': {
+        return data.founding_document ? data.founding_document.type : 'null';
+      }
+      case 'base': {
+        return data.reference ? data.reference.type : 'null';
+      }
+    }
   }
 
-  refreshData() {
+  ngOnInit() {
     this.auditservice.getViolations(this.idAudit).subscribe(data => {
-      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.sortingDataAccessor = this._sortingDataAccessor;
+      this.dataSource.sort = this.sort;
+      this.dataSource.data = data;
+      this.changeDetectorRefs.detectChanges();
     });
   }
 }
